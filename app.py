d82aab8778540e5abed7f8dfcd3af59c8321962d
Justin Sin <48116685+Jvil26@users.noreply.github.com>
2023-02-06 15:30:02 -0800

    Fix SQL Queries

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, request
-import datetime
+from datetime import datetime, timezone
 from sql_queries import *
 from webscrape import *
 
@@ -17,21 +17,22 @@ def create_search():
     data = request.get_json()
     name = data["name"]
     try:
-        info = scrape_url()
-        if info["is_processed"]:
-            with connection:
-                with connection.cursor() as cursor:
-                    cursor.execute(CREATE_SEARCHES_TABLE)
-                    cursor.execute(FIND_SEARCH, (name,))
-                    search_with_name = cursor.fetchone()
-                    if search_with_name:
-                        search_count = search_with_name[3]
-                        cursor.execute(UPDATE_SEARCH, (datetime.date.today.strftime("%B %d, %Y"), search_count + 1, name))
-                    else:
-                        cursor.execute(INSERT_SEARCH, (name, 0, datetime.date.today.strftime("%B %d, %Y")))
-        return { "success": True, "data": info }
-    except:
-        return { "error": True }
+        # info = scrape_url()
+        curr_date = datetime.now(timezone.utc)
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(CREATE_SEARCHES_TABLE)
+                cursor.execute(FIND_SEARCH, (name,))
+                search_with_name = cursor.fetchone()
+                if search_with_name:
+                    search_count = search_with_name[3]
+                    cursor.execute(UPDATE_SEARCH, (curr_date, search_count + 1, name))
+                else:
+                    cursor.execute(INSERT_SEARCH, (name, 0, curr_date))
+                response = cursor.fetchone()
+        return { "success": True, "search": response }, 200
+    except Exception as e:
+        return { "error": e }
 
 @app.get("/")
 def home():
--- sql_queries.py
+++ sql_queries.py
@@ -1,13 +1,15 @@
 CREATE_SEARCHES_TABLE = (
-    "CREATE TABLE IF NOT EXISTS searches (id SERIAL PRIMARY KEY, date TIMESTAMP, name TEXT, search_count INTEGER"
+    "CREATE TABLE IF NOT EXISTS searches (id SERIAL PRIMARY KEY, date TIMESTAMP, name TEXT, search_count INTEGER)"
 )
 
-INSERT_SEARCH = "INSERT INTO searches (name, search_count, date) VALUES (%s, %d, %s)"
+INSERT_SEARCH = "INSERT INTO searches (name, search_count, date) VALUES (%s, %s, %s) RETURNING id, name, search_count, date"
 
 MOST_SEARCHED = "SELECT * from searches order by search_count desc limit 10"
 
 MOST_RECENT_SEARCHES = "SELECT * from searches order by date desc limit 10"
 
-FIND_SEARCH = "SELECT * from searches WHERE searches.name = (%s)"
+FIND_SEARCH = "SELECT * from searches WHERE name = (%s)"
 
-UPDATE_SEARCH = "UPDATE searches SET date = %s, search_count = %d WHERE name = %s"
+UPDATE_SEARCH = "UPDATE searches SET date = %s, search_count = %s WHERE name = %s RETURNING id, name, search_count, date"
+
+DELETE_SEARCHES = "DELETE FROM searches"
