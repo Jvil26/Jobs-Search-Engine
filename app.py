3ac8d17b7bf105b60a521988f110927e89500062
Justin Sin <48116685+Jvil26@users.noreply.github.com>
2023-03-24 02:37:49 -0700

    Scrape jobs function

--- app.py
+++ app.py
@@ -15,24 +15,24 @@ connection = psycopg2.connect(url)
 @app.post("/api/search")
 def create_search():
     data = request.get_json()
-    name = data["name"]
+    job_category = data["job_category"]
     try:
-        # info = scrape_url()
+        info = scrape_jobs(job_category)
         curr_date = datetime.now(timezone.utc)
         with connection:
             with connection.cursor() as cursor:
                 cursor.execute(CREATE_SEARCHES_TABLE)
-                cursor.execute(FIND_SEARCH, (name,))
+                cursor.execute(FIND_SEARCH, (job_category,))
                 search_with_name = cursor.fetchone()
                 if search_with_name:
                     search_count = search_with_name[3]
-                    cursor.execute(UPDATE_SEARCH, (curr_date, search_count + 1, name))
+                    cursor.execute(UPDATE_SEARCH, (curr_date, search_count + 1, job_category))
                 else:
-                    cursor.execute(INSERT_SEARCH, (name, 0, curr_date))
+                    cursor.execute(INSERT_SEARCH, (job_category, 0, curr_date))
                 response = cursor.fetchone()
-        return { "success": True, "search": response }, 200
+        return { "success": True, "search": response, "job_info": info }, 201
     except Exception as e:
-        return { "error": e }
+        return { "error": str(e) }
 
 @app.get("/")
 def home():
--- webscrape.py
+++ webscrape.py
@@ -1,6 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-def scrape_url(url):
-    result = requests.get(url)
-    doc = BeautifulSoup(result.content, "html.parser")
+BASE_URL = 'https://www.linkedin.com/jobs/'
+CARD_CLASS = "base-card"
+ROLE_CLASS = "base-search-card__title"
+INFO_LINK_CLASS = "base-card__full-link"
+LOCATION_CLASS = "job-search-card__location"
+COMPANY_CLASS = "hidden-nested-link"
+LISTDATE_CLASS = "job-search-card__listdate"
+
+def scrape_jobs(job_category):
+    info = {}
+    result = requests.get(BASE_URL + job_category + '-jobs/')
+    doc = BeautifulSoup(result.text, 'html.parser')
+    if not doc.main:
+        raise Exception("Error getting jobs")
+    job_cards = doc.main.find_all(class_=CARD_CLASS)
+    for i in range(len(job_cards)):
+        print(job_cards[i])
+        internship = {
+            "role": job_cards[i].find(class_=ROLE_CLASS).text.strip(), 
+            "link": job_cards[i].find(class_=INFO_LINK_CLASS)["href"],
+            "location": job_cards[i].find(class_=LOCATION_CLASS).text.strip(),
+            "list_date": job_cards[i].find(class_=re.compile(f"^{LISTDATE_CLASS}")).text.strip()
+        }
+        company = job_cards[i].find(class_=COMPANY_CLASS).text.strip()
+        info[company] = internship
+    print(info)
+    return info
+    
+
+if __name__ == "__main__":
+    scrape_jobs("computer-science-intern")
